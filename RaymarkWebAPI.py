4e878163c8a9c63525a745174f1661e4e9526b89
Sandeep Arcot <sandeep.arcot@gmail.com>
2018-12-03 16:54:21 +0800

    Added the class for fetching the quantity

--- RaymarkWebAPI.py
+++ RaymarkWebAPI.py
@@ -4,27 +4,33 @@ from sqlalchemy import create_engine
 from json import dumps
 from flask_jsonpify import jsonify
 import pyodbc
-cnxn = pyodbc.connect("Driver={SQL Server Native Client 11.0};"
-                      "Server=HK-KWN-RPTST01\IZ_UAT;"
-                      "Database=ELHK_UAT;"
-                      "Trusted_Connection=yes;")
+
+app = Flask(__name__)
+api = Api(app)
 
 
-cursor = cnxn.cursor()
-cursor.execute("select top 100 rtrim(store_code) StoreCode,rtrim(product_code) ProductCode,sum(isnull(quantity_received,0) - isnull(quantity_sold,0) -isnull(quantity_return_vendor,0) + isnull(qty_distributed_in, 0) - isnull(qty_distributed_out, 0) + isnull(qty_transfered_in, 0) - isnull(qty_transfered_out, 0) + isnull(qty_adj_inv_atl, 0 ) + isnull(quantity_returned,0) + isnull(shrink_qty, 0) + isnull(qty_adj_inv_btl, 0 )) QtyOnHand "
+class Quantity(Resource):
+    def get(self,pcode):
+        cnxn = pyodbc.connect("Driver={SQL Server Native Client 11.0};"
+                              "Server=HK-KWN-RPTST01\IZ_UAT;"
+                              "Database=ELHK_UAT;"
+                              "Trusted_Connection=yes;")  # Connect to the database
+        cursor = cnxn.cursor()
+
+        query = cursor.execute("select top 100 rtrim(store_code) StoreCode,rtrim(product_code) ProductCode,cast(sum(isnull(quantity_received,0) - isnull(quantity_sold,0) -isnull(quantity_return_vendor,0) + isnull(qty_distributed_in, 0) - isnull(qty_distributed_out, 0) + isnull(qty_transfered_in, 0) - isnull(qty_transfered_out, 0) + isnull(qty_adj_inv_atl, 0 ) + isnull(quantity_returned,0) + isnull(shrink_qty, 0) + isnull(qty_adj_inv_btl, 0 ))as int) QtyOnHand "
                "from ELHK_UAT..product_now pn (nolock) inner join ELHK_UAT..store s (nolock)on pn.store_code_id = s.store_code_id "
                "inner join ELHK_UAT..product p (nolock) "
                "on p.Product_id = pn.product_id "
-               "where s.store_code = 'CL52' and p.product_code = 'ZTPH02'"
-               "group by store_code, product_code" % str(store_id)), % str(employee_id))
+               "where s.store_code = 'CL52' and p.product_code = '%s'"
+               "group by store_code, product_code;" % str(pcode))# This line performs query and returns json result
 
-for row in cursor:
-    print('row = %r' % (row,))
+        result = {'qtyonhand': [i[2] for i in query.fetchall()]}  # Fetches
+        print(result)
+        return jsonify(result)
+
+api.add_resource(Quantity, '/qtyonhand/CL52/<pcode>')  # Route_1
 
 
-    class OnHandInventory(Resource):
-        def get(self, store_id,sku):
-            conn = db_connect.connect()
-            query = conn.execute("select * from employees where EmployeeId =%d " % int(employee_id))
-            result = {'data': [dict(zip(tuple(query.keys()), i)) for i in query.cursor]}
-            return jsonify(result)
+if __name__ == '__main__':
+    app.run(port='5007')
+
