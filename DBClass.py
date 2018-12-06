ca1642f6c23d3e0cacd74f5d9c129062f7e24896
Sandeep Arcot <sandeep.arcot@gmail.com>
2018-12-06 14:56:29 +0800

    Added Validations and handled exceptions

--- DBClass.py
+++ DBClass.py
@@ -7,25 +7,40 @@ class dbconnect:
 #function for creating a database connection to SQL Server. The arguments are server name, database name , username and password.
 
     def sqlconnect(self,server,dbname,uname,pwd):
-        cnxn = pyodbc.connect("Driver={SQL Server Native Client 11.0};"
+        try:
+            cnxn = pyodbc.connect("Driver={SQL Server Native Client 11.0};"
                               "Server=%s;"
                               "Database=%s;"
                               "Trusted_Connection=yes;"  % (str(server),str(dbname)))
-        return cnxn
+            return cnxn
+        except pyodbc.Error as ex:
+            sqlstate = ex.args[0]
+            #if sqlstate == '08001':
+            print ex.args[1]
+
+
 
 #function returns query result in a cursor. The arguments are connection and sql query
 
     def getdata(self,cnxn,query):
         cursor = cnxn.cursor()
-        cursor.execute(query)
-        return cursor
+        try:
+            cursor.execute(query)
+            return cursor
+        except pyodbc.Error as ex:
+            sqlstate = ex.args[0]
+            # if sqlstate == '08001':
+            print ex.args[1]
 
-C1 = dbconnect()
+    def validatestoreCode(self,cnxn,scode):
+            try:
+                cursor = cnxn.cursor()
+                cursor.execute("Select rtrim(store_code) StoreCode from ELHK_UAT..store s where s.store_code = '%s'" % str(scode))
+                results = cursor.fetchall()
+                if results == []:
+                    return False
+                else:
+                    return True
+            except pyodbc.Error as ex:
+                    print ex.args[1]
 
-conn = C1.sqlconnect("HK-KWN-RPTST01\IZ_UAT","ELHK_UAT","abc","pwd")
-query = "select rtrim(store_code) StoreCode,rtrim(product_code) ProductCode,sum(isnull(quantity_received,0) - isnull(quantity_sold,0) -isnull(quantity_return_vendor,0) + isnull(qty_distributed_in, 0) - isnull(qty_distributed_out, 0) + isnull(qty_transfered_in, 0) - isnull(qty_transfered_out, 0) + isnull(qty_adj_inv_atl, 0 ) + isnull(quantity_returned,0) + isnull(shrink_qty, 0) + isnull(qty_adj_inv_btl, 0 )) QtyOnHand from ELHK_UAT..product_now pn (nolock) inner join ELHK_UAT..store s (nolock)on pn.store_code_id = s.store_code_id inner join ELHK_UAT..product p (nolock) on p.Product_id = pn.product_id where s.store_code = 'CL52' and p.product_code = 'ZTPH02'group by store_code, product_code"
-print (query)
-
-cursor = C1.getdata(C1.sqlconnect("HK-KWN-RPTST01\IZ_UAT","ELHK_UAT","abc","pwd"),query)
-for row in cursor:
-    print('row = %r' % (row,))
--- RaymarkWebAPI.py
+++ RaymarkWebAPI.py
@@ -1,6 +1,6 @@
 from flask import Flask, request
 from flask_restful import Resource, Api
-from sqlalchemy import create_engine
+#from sqlalchemy import create_engine
 from json import dumps
 from flask_jsonpify import jsonify
 import pyodbc
@@ -12,15 +12,28 @@ api = Api(app)
 
 class Quantity(Resource):
     def get(self,scode,pcode):
-        v_dbobj = dbconnect()
+        v_dbobj = dbconnect()  # Creating the instance of the dbconnect class from DBClass.py
         conn = v_dbobj.sqlconnect("HK-KWN-RPTST01\IZ_UAT", "ELHK_UAT", "abc", "pwd")
+
+        # Validating the length of SKU
+        if len(pcode) <> 6:
+            return {'Error':['6 digit SKU only allowed']}
+
+        # Validating the Store Code
+        if (v_dbobj.validatestoreCode(conn,scode)):
+            pass
+        else:
+            return {'Error': ['Not a Valid StoreCode']}
+
 
         query = "select rtrim(store_code) StoreCode,rtrim(product_code) ProductCode,cast(sum(isnull(quantity_received,0) - isnull(quantity_sold,0) -isnull(quantity_return_vendor,0) + isnull(qty_distributed_in, 0) - isnull(qty_distributed_out, 0) + isnull(qty_transfered_in, 0) - isnull(qty_transfered_out, 0) + isnull(qty_adj_inv_atl, 0 ) + isnull(quantity_returned,0) + isnull(shrink_qty, 0) + isnull(qty_adj_inv_btl, 0 ))as int) QtyOnHand from ELHK_UAT..product_now pn (nolock) inner join ELHK_UAT..store s (nolock)on pn.store_code_id = s.store_code_id inner join ELHK_UAT..product p (nolock) on p.Product_id = pn.product_id where s.store_code = '%s' and p.product_code = '%s'group by store_code, product_code" % (str(scode), str(pcode))
 
         cursor = v_dbobj.getdata(conn,query)
+        if cursor is None:
+            return {'Error':['No Data']}
 
         for i in cursor.fetchall():
-            result = {'Store Code': [i[0]],'6 digit SKU': [i[1]], 'Quantity': [i[2]]}
+            result = {'Error':[],'Store Code': [i[0]],'6 digit SKU': [i[1]], 'Quantity': [i[2]]}
 
         print(result)
         return jsonify(result)
